11a79dd71a4c2db1233fefb55ccf315ab43bd0e5
noam3127 <noshev27@gmail.com>
2013-11-19 02:20:41 -0500

    saved favorite to session

--- viewCharacteristics.php
+++ viewCharacteristics.php
@@ -6,18 +6,30 @@ include_once('characteristics.php');
 class viewCharacteristics extends view {
 	
 	public function content() {
+		
 		$characteristics = new characteristics; 
 		$ID = $_REQUEST['inst'];
 		//print_r($ID);
 		$results = characteristics::instChar($ID);
 		$name = characteristics::setup($results);
-			?> 	<button type="button" class="btn btn-info">Compare side by side with other institutions</button>
-			<div class="col-md-3">
+			?> 	
+			
+			<div class="col-md-3 col-md-offset-7">
+				<form method="post" action="">
+					<input type="submit" class="btn btn-info" value="Save this institution for comparison" name="compare">
+				</form>	 
+				
+				<form action="viewFavorites.php" method="post">
+					<input type="button" class="btn btn-warning" name="saved" value="View my saved colleges"> 
+				</form>
+			</div>
+		<div class="col-md-3">
 			<table class ="table table-striped table-bordered table-condensed ">
 					
 				<th>
 					<tr>
 		 <?php
+		 
 		 foreach($results as $k=>$sub){
 					unset($results[$k]['instName']);	
 		 }
@@ -32,8 +44,18 @@ class viewCharacteristics extends view {
 				}					
 		  }
 				
-			echo "</div></table>";
+			echo "</div></table> </form>";
 			
+			
+	if(isset($_POST['compare'])){	
+		session_start();
+		
+		if(in_array($ID, $_SESSION['comp']) == FALSE){
+			$_SESSION['comp'][] = $ID;
+		}
+		print_r($_SESSION); 
+		header('Location:viewSearch');
+		}	
     }
 	
 }
