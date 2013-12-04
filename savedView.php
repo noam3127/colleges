57fd89ea9bcdafe879fafe972189bf5d00ddb7b6
noam3127 <noshev27@gmail.com>
2013-12-03 20:18:03 -0500

    favorate list

--- savedView.php
+++ savedView.php
@@ -15,9 +15,33 @@ class savedView extends view {
 			</div>
 		</div>
 <?php		
+	session_start();	
+	$saved = new saved;
+	
+	if(array_key_exists('comp', $_SESSION)){
+		$favorites = $saved->getFavorites();
+		?>
+		<h4 class="col-md-5 col-md-offset-1">
+		<?php	
+		$n = 1;
+		$remove = "<input type='checkbox' value='remove'>";
+		//print_r($favorites);
+		echo "<table class ='table table-striped table-bordered table-condensed'>
+				<thead><span class='lead'>Saved Institutions</span></thead> <tr>";
+		foreach($favorites as $k=>$sub){
+			//echo "<div class='lead'> remove";	  
+			array_unshift($sub,$remove);
+			foreach($sub as $field=>$val){
+				
+				echo '<td>'. $val. '  ';			
+			}
+			echo"</tr>";
+		}
+		echo"</table> </h4>";
+		//<br>
+		//<input type="submit" value = "remove" name "remove"> </input>
 		
-		$saved = new saved;
-					
+	//<?php
 		$finance = $saved->getFinance();
 		//unset($finance['instID']);
 		$financeFields = array(array('Name','ID','Total assets','Total liabilities','Net assets',
@@ -32,21 +56,29 @@ class savedView extends view {
 			}
 		}
 	//	print_r($ordered);
-	?>		<div class"container">
-				<div class="col-sm-12">
-					<h4>Financial Data for 2011</h4>
+	?>	
+		<div class"container">
+				<div class="col-sm-10 col-sm-offset-1">
+					<span class="lead">Financial Data for 2011</span>
 			<table class ="table table-striped table-bordered table-condensed ">
-  <?php	   $i = 1;		
+  <?php	   $i = 1;	
+  		  
 					 foreach($finance as $sub){
+					 	 $n = 1;	
 						echo"<tr>";	 			               
 						foreach($sub as $val){
 							 if($i<=count($sub)){
-						 	echo "<td><h5>$val</h5></td>";
-							 $i++;
-						 } 	else{	
-							echo "
-						   		<td>$val</td>";	
-						 }					   							    	 
+						 	    echo "<td class='warning'><h5>$val</h5></td>";
+							    $i++;
+						 } 	elseif($n<=2){	
+							    echo "
+						   		  <td>$val</td>";	
+								  $n++;
+						 }	else {
+						 		$val = '$'. number_format($val); 
+						 	    echo "
+						 	    	<td>$val</td>";
+						 }				   							    	 
 						}
 						echo "</tr>";
 				     }				 		
@@ -66,26 +98,40 @@ class savedView extends view {
 			'Percent of undergraduates receiving Pell grants','Total Pell grants','Avg. Pell grant per undergrad',
 			'Percent of undergraduates receiving federal grants','Total federal grants','Avg. federal grant per undergrad'));
 		$aidFields = array_splice($aid, 0,0, $aidFields);	
-	?>	<div class="col-sm-12">
-				<table class ="table table-striped table-bordered table-condensed ">
-					<h4>Aid received for 2011-2012</h4>
+	?>	<br><div class="col-sm-10 col-sm-offset-1">
+				<table class ="table table-striped table-bordered table-condensed">
+					<span class="lead">Aid received for 2011-2012</span>
 	<?php 
-		 
+	
+		 $i = 0;
 					 foreach($aid as $sub){
-						$i = 0;	
+						$n = 1;	
 						echo"<tr>";	
 						               
 						foreach($sub as $val){
+							$i++;
 							 if($i<=count($sub)){
-						 	echo "<td><h5>$val</h5></td>";
+						 	   echo "<td class='warning'><h5>$val</h5></td>";
 							
-						 } 	else{
-						 	 $i++;
-						 	 
-						 	//$val = ($i==1 ? $val: number_format($val));		               
-							echo "
+						     }elseif($n<=2){
+						 	   echo "
 						   		<td> $val</td>";	
-						 }					   							    	 
+						 	  
+						     } else{
+						 		//$n=3;	
+								//$n++; 
+						 	    $val = number_format($val);
+								
+								if($n!==3 && $n!==6 && $n!==9){
+									$val = '$'. $val;
+								}elseif($n==6 || $n==9)	{
+									$val = $val .'%';
+								}               
+							    echo "
+						   		<td> $val</td>";
+									
+						   }
+					   		 $n++;					    	 
 						}
 
 						echo "</tr>";
@@ -97,6 +143,10 @@ class savedView extends view {
 			
 				
 	<?php	
+	  } else {
+	  	echo"<h3>You have no saved institutions.</h3>";
+	  }
+	 
 	}					
 	
 }
