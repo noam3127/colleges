c9d29f8d5f4f4cb610467f776bfa0fc90c63fe5d
noam3127 <noshev27@gmail.com>
2013-12-13 03:08:47 -0500

    general info query

--- characteristics.php
+++ characteristics.php
@@ -2,47 +2,50 @@
 
 include_once('database.php');
 
-class characteristics{
+class characteristics {
+	
+	public static $rows = array();
 	
 	public function __construct(){
-		if(isset($_REQUEST['inst'])){
-			$ID = $_REQUEST['inst'];
-			echo "hello $ID";
-		}
 		
-	    $select = 'SELECT * FROM finance2011 f INNER JOIN aid2012 a ON f.instID=a.instID WHERE f.instID = :ID ';
-		$db = database::getDB();
+	}
+	
+	public function getGeneralInfo($ID){
+		$selectGeneralInfo = 'SELECT instName, address, city, state, zip, webAddress FROM generalInfo2012 WHERE instID = :ID ';
+		$results = $this->query($selectGeneralInfo, $ID);
+		return $results;
+	}
+	public function instChar($ID){
+		
+		//session_start();
+	    $select = 'SELECT g.instName, f.*, a.* FROM finance2011 f INNER JOIN aid2012 a ON f.instID=a.instID 
+	    	INNER JOIN generalInfo2012 g ON a.instID = g.instID WHERE f.instID = :ID ';
+		 $results = $this->query($select, $ID);
+		self::$rows = $results;
+		return $results;
+	
+	}
+
+	public function query($select, $ID){
+		$db = database::getDB(); 
 		$STH = $db->prepare($select);
 		$STH->bindParam(':ID', $ID);
 		$STH->execute();
 		$STH->setFetchMode(PDO::FETCH_ASSOC); 
 		$results = $STH->fetchAll();
-		//print_r($results); 
-		$this->showResults($results);
+		return $results;
 	}
-	protected function showResults($results){
+	
+	public static function setup($results){
 		
-		echo "<table>
-				<th><tr>";
-					foreach($results as $sub){
-						foreach($sub as $col=>$v){
-							echo "<td> $col</td>";	
-						}
-					
-					}
-			echo "</tr></th>";
-		foreach ($results as $k=>$v) {
-			
-			echo "<tr>";
-			 foreach($v as $col=>$val){
-	      
-	  			 echo '<td>'. $val . '</td>';
-			}		
-		}	
-		echo "</tr> 
-				</table>"; 	
+		foreach($results as $k=>$sub){
+							
+			$name= $sub['instName'];
+			unset($results[$k]['instName']);			
+		}
+		
+		return $name;
 	}
 }
 
-$characteristics = new characteristics;
 ?>
