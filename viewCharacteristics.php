77d10877edff616572b6f7c14ac941ddde527d32
noam3127 <noshev27@gmail.com>
2013-12-18 18:46:37 -0500

    web-link button

--- viewCharacteristics.php
+++ viewCharacteristics.php
@@ -13,38 +13,40 @@ class viewCharacteristics extends view {
 		$generalInfo = $characteristics->getGeneralInfo($ID);
 		$results = $characteristics->instChar($ID);
 		$name = characteristics::setup($results);
-			?> 	
-			
-			<div class="col-md-3 col-md-offset-7">
-				<form method="post" action="">
-					<input type="submit" class="btn btn-info btn-block " value="Save this institution for comparison" name="compare">
-				</form>	 
-				<br>
-				
-				<a href="savedView.php"><button class="btn btn-warning btn-block">View my saved colleges</button></a>
-			</div>
-		<div class= "col-md-10">
+	?> 	
+		<div class="container col-md-offset-1">
+		<div class= "col-md-3" style="border: 2px solid black; border-radius: 8px; float:left;">
 	<?php 
-	//print_r($generalInfo);
+
 		foreach($generalInfo as $k=>$v){
 			extract($v);
-			foreach($v as $key => $val){
-			   
-			   echo $val . "<br> ";
-			} 
 		}
 		
-		// echo $generalInfo;
-	?>	
-	</div>	
-
-		<div class="col-md-3">
-			<?php //echo "<h3> $name </h3>"; ?>
-			<table class ="table table-striped table-bordered table-condensed ">
-					
-				<th>
-					<tr>
-		 <?php
+		echo "<h2>";
+		echo $instName . "</h2>";
+		echo  '<h4 class = "lead">' . $address . '<br />' . $city . ', ' . $state . ' <br />' . $zip . '</h4>';
+		if($webAddress != " "){
+	?>		
+			<a href= http://<?php echo $webAddress; ?>  target='_blank'>
+			<h3 style='border: 1px solid darkgray; border-radius: 8px; width: 9em; text-align:center; background: linear-gradient(to top, gray 0%,lightgray 40%); '>Visit website</h3></a> 
+<?php   }
+?>		
+	  </h3>
+	
+	</div>
+	
+	<form method="post" action="" style="clear:both; width: 19em; padding-top: 2em;float:left;">
+		<input type="submit" class="btn btn-info btn-block " value="Save this institution for comparison" name="compare">
+	</form>	 
+		
+	<div class="col-md-3" style="">	
+		<a href="savedView.php"><button class="btn btn-warning btn-block">View my saved colleges</button></a>
+	</div>
+	
+	<div class="col-md-5" style="">
+		<table class ="table table-striped table-bordered table-condensed ">
+			<tr>				
+	<?php
 		
 		 foreach($results as $k=>$sub){
 					unset($results[$k]['instName']);	
@@ -63,21 +65,18 @@ class viewCharacteristics extends view {
 			echo "</div></table> </form>";?>
 			<div id="map-canvas"/>
 			</div>
-			</div>
-<?php			
 			
-	if(isset($_POST['compare'])){	
-		session_start();
-		
-		if(in_array($ID, $_SESSION['comp']) == FALSE){
-			$_SESSION['comp'][] = $ID;
-		}
-		//print_r($_SESSION); 
-		header('Location:viewSearch');
-		}	
-	  }
-    
+			</div>
 	
+	<?php
+		if(isset($_POST['compare'])){	
+			session_start();
+			
+			if(in_array($ID, $_SESSION['comp']) == FALSE){
+				$_SESSION['comp'][] = $ID;
+			}
+		}	
+    }	
 }
 $view = new viewCharacteristics;
 $view->content();
