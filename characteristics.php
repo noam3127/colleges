1681042fc3a45884d87d83fc0ad98ca00641199c
noam3127 <noshev27@gmail.com>
2013-12-18 18:46:04 -0500

    field names within select statement

--- characteristics.php
+++ characteristics.php
@@ -18,7 +18,14 @@ class characteristics {
 	public function instChar($ID){
 		
 		//session_start();
-	    $select = 'SELECT g.instName, f.*, a.* FROM finance2011 f INNER JOIN aid2012 a ON f.instID=a.instID 
+	    $select = 'SELECT g.instName, f.instID as ID, a.undergradTotal as "Total undergraduates", f.totalAssets as "Total Assets",
+	    	f.totalLiab as "Total Liabilities", f.netAssets as "Net Assets", f.netTuitionFees as "Net amount of tuition and fees",
+	    	f.totalRevIncome as "Net revenue and income", a.grantTotal as "Total amount of grants received",
+	    	a.grantAvg as "Avg. grant amount per student", a.undergradPercentPell "Percent of undergrads receiving Pell grants",
+	    	a.totalPell as "Total amount of Pell grants", a.avgPell as "Avg. Pell grant per undergrad",
+	    	a.undergradPercentFed as "Percent of undergrads receiving Federal grants", a.totalFed as "Total federal grants",
+	    	a.avgFed as "Avg. federal grant per undergrad" 
+	    	FROM finance2011 f INNER JOIN aid2012 a ON f.instID=a.instID 
 	    	INNER JOIN generalInfo2012 g ON a.instID = g.instID WHERE f.instID = :ID ';
 		 $results = $this->query($select, $ID);
 		self::$rows = $results;
